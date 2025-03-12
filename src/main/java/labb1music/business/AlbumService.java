faf0ca58a5bb21a12cd92679e316877b175cb65d
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-03-12 20:01:58 +0100

    add some code to AlbumService

--- src/main/java/labb1music/business/AlbumService.java
+++ src/main/java/labb1music/business/AlbumService.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import labb1music.AlbumRepository;
 import labb1music.dto.AlbumResponse;
 import labb1music.dto.CreateAlbum;
+import labb1music.dto.UpdateAlbum;
 import labb1music.entity.Album;
 import labb1music.exceptions.NotFound;
 import labb1music.mapper.AlbumMapper;
@@ -42,11 +43,27 @@ public class AlbumService {
                         () -> new NotFound("Album with id " + id + " not found!")
                 );
     }
-    
-    public Album createAlbum (CreateAlbum album) {
+
+    public Album createAlbum(CreateAlbum album) {
         Album newAlbum = AlbumMapper.map(album);
         newAlbum = repository.save(newAlbum);
         return newAlbum;
+    }
+
+    public void updateAlbum(Long id, UpdateAlbum album) {
+        var oldAlbum = repository.findById(id).orElseThrow(() -> new NotFound("Album with id " + id + " not found!"));
+        if (album.title() != null)
+            oldAlbum.setTitle(album.title());
+        if (album.artist() != null)
+            oldAlbum.setArtist(album.artist());
+        if (album.genre() != null)
+            oldAlbum.setGenre(album.genre());
+        if (album.trackCount() != 0)
+            oldAlbum.setTrackCount(album.trackCount());
+        if (album.releaseYear() != 0)
+            oldAlbum.setReleaseYear(album.releaseYear());
+        repository.save(oldAlbum);
+
     }
 
 }
