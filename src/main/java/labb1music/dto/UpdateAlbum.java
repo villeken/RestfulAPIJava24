ac606617bd17323954b5ab21d8cc5c1da1dc4d8e
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-03-14 22:37:41 +0100

    Change some code in updateAlbum

--- src/main/java/labb1music/dto/UpdateAlbum.java
+++ src/main/java/labb1music/dto/UpdateAlbum.java
@@ -1,5 +1,13 @@
 package labb1music.dto;
 
-public record UpdateAlbum(Long id, String title, String artist, String genre, Integer trackCount, Integer releaseYear) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record UpdateAlbum(@NotBlank @NotNull(message = "Title is mandatory") String title,
+                          @NotBlank @NotNull (message = "Artist is mandatory") String artist,
+                          @Positive @NotBlank @NotNull (message = "Release year is mandatory and can not be less than 0") Integer releaseYear,
+                          @Positive @NotBlank @NotNull (message = "Track count is mandatory and can not be less than 0") Integer trackCount,
+                          @NotBlank @NotNull (message = "Genre is mandatory") String genre) {
 
 }
